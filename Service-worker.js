3a397200135c716cc462b28dcd2b2359d8618e5b
mariale1836 <mariale1836fyd@gmail.com>
2026-08-25 21:11:24 -0400

    Create Service-worker.js

---
+++ Service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "social-ai-studio-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./video-providers.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
